434c6b64878b90c8563541e11cd6df60913e9859
Alzu <albertitopkr@gmail.com>
2025-11-03 23:07:28 -0300

    version inicial de veterinaria-api

--- src/mascotas/mascotas.controller.ts
+++ src/mascotas/mascotas.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { MascotasService } from './mascotas.service';
 
 @Controller('mascotas')
-export class MascotasController {}
+export class MascotasController {
+  constructor(private readonly mascotasService: MascotasService) {}
+
+  @Get()
+  findAll() {
+    return this.mascotasService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.mascotasService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() body) {
+    return this.mascotasService.create(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.mascotasService.update(Number(id), body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.mascotasService.remove(Number(id));
+  }
+}
